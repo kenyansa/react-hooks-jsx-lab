049a405e1171e4e3eda52682b8ce723acfef933b
kenyansa <kenyansajustine@gmail.com>
2022-05-05 00:18:29 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>
+      I reallly love coding of late and I want to be the best programmer Kenya can afford to solve as many logical problems as possible.
+    </p>
+    <img src={image} alt="I made this"></img>
+
+  </div>
+  );
 }
 
 export default About;
